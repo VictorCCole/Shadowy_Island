9b2905d092f7b757b99e37413bb057781a54d1d0
EduardoHenn67 <eduardohenn89@gmail.com>
2022-04-11 10:26:24 -0300

    Moviment

--- Game/Assets/CharacterController2D.cs
+++ Game/Assets/CharacterController2D.cs
@@ -8,9 +8,11 @@ public class CharacterController2D : MonoBehaviour
    Rigidbody2D rigidbody2d;
    [SerializeField] float speed = 2f;
    Vector2 motionVector;
+   Animator animator;
     void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -19,6 +21,8 @@ public class CharacterController2D : MonoBehaviour
             Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical")
             );
+        animator.SetFloat("horizontal",Input.GetAxisRaw("Horizontal"));
+        animator.SetFloat("vertical",Input.GetAxisRaw("Vertical"));
     }
 
     void FixedUpdate()
